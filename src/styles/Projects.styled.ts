16f4a6ce13bfa82d2d848a42d5948c5526548f4a
dziubenzo <dziubenzo@gmail.com>
2024-08-12 15:36:15 +0200

    Figure out buttons on top of div with screenshots
    That took way too long. In the end, I had to use a very inconspicuous
    solution, but it should be responsive.

--- src/styles/Projects.styled.ts
+++ src/styles/Projects.styled.ts
@@ -3,11 +3,21 @@ import styled from 'styled-components';
 export const StyledProjects = styled.section``;
 
 export const StyledProject = styled.div`
+  position: relative;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  
+  grid-template-columns: 2fr 1fr;
+  gap: 16px;
+
+  .selector-wrapper {
+    position: absolute;
+    height: 100%;
+    width: calc(100% / 3 * 2); // 2fr
+    background: none;
+    pointer-events: none;
+  }
+
   .screenshots-wrapper {
-    overflow-x: auto;
+    overflow-x: hidden;
 
     .horizontal-scroll-wrapper {
       display: flex;
@@ -20,6 +30,60 @@ export const StyledProject = styled.div`
           width: 100%;
           border-radius: 8px;
         }
+      }
+    }
+  }
+
+  .project-name-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+
+    h1 {
+      font-size: ${(props) => props.theme.fontSizes.extraLarge};
+      font-family: ${(props) => props.theme.fonts.secondary};
+    }
+  }
+`;
+
+export const StyledScreenshotSelector = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 90%;
+  transform: translate(-50%, -50%);
+  background: transparent;
+  pointer-events: auto;
+
+  button {
+    font-size: ${(props) => props.theme.fontSizes.extraLarge};
+    width: 3ch;
+    border: none;
+    color: white;
+    background-color: black;
+    cursor: pointer;
+
+    &.active {
+      color: black;
+      background-color: white;
+    }
+  }
+
+  button:first-of-type {
+    border-radius: 16px 0 0 16px;
+  }
+
+  button:last-of-type {
+    border-radius: 0 16px 16px 0;
+  }
+
+  @media (hover: hover) {
+    button {
+      transition: all 0.15s ease-in-out;
+
+      &:hover {
+        color: black;
+        background-color: white;
       }
     }
   }
