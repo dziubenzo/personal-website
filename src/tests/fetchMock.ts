eca0747278d40906351a21a22d8559461c0cb9f3
dziubenzo <dziubenzo@gmail.com>
2025-08-13 17:04:53 +0200

    Remove testing deps

--- src/tests/fetchMock.ts
+++
@@ -1,9 +0,0 @@
-export const mockFetch = (data: object, ok: boolean) => {
-  return vi.spyOn(global, 'fetch').mockImplementation(() =>
-    Promise.resolve({
-      ...new Response(),
-      json: () => Promise.resolve(data),
-      ok: ok,
-    }),
-  );
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
-  },
 });
